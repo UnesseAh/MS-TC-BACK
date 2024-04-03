1314fc076efa0f8e86f992ff6262cac24b08ba81
UnesseAh <youness.ahasla@gmail.com>
2024-04-03 12:10:15 +0000

    allocate exception handling in controller advice

---
+++ ms-tc-reservation/src/main/java/com/tourconnect/mctcreservation/handler/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.tourconnect.mctcreservation.handler.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
